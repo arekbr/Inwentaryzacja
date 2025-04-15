85e121920492f7819f22d9cc74a11c19c640ebe5
Yugorin <arek@bronowicki.com>
2025-04-15 20:14:47 +0200

    ikony do programu i pliku wykonywalnego

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
+    a.setWindowIcon(QIcon(":/icon.png"));
     // Instalacja tłumaczeń (jeśli są dostępne)
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
