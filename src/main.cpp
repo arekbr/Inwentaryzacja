ab9630df993c07b2c5b43a83c70c98997027bf19
Yugorin <arek@bronowicki.com>
2025-05-01 17:36:43 +0200

    przeniosłem skórkę do dialogu z wyborem bazy danych

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,19 @@
  * @file main.cpp
  * @brief Główny punkt wejścia aplikacji inwentaryzacyjnej.
  * @author Stowarzyszenie Miłośników Oldschoolowych Komputerów SMOK & ChatGPT & GROK
- * @version 1.1.10
- * @date 2025-04-30
+ * @version 1.2.0
+ * @date 2025-05-01
  *
  * Plik zawiera funkcję main, która inicjalizuje aplikację Qt, ustawia nazwę i wersję aplikacji,
  * ładuje tłumaczenia, wyświetla okno konfiguracji bazy danych (SQLite lub MySQL), nawiązuje
- * połączenie z bazą danych za pomocą funkcji setupDatabase, ładuje skórkę graficzną z QSettings
- * i uruchamia główne okno aplikacji (klasa itemList). Czcionki są zarządzane przez klasę itemList.
+ * połączenie z bazą danych za pomocą funkcji setupDatabase i uruchamia główne okno aplikacji
+ * (klasa itemList). Skórka graficzna i czcionki są zarządzane przez klasę DatabaseConfigDialog.
  */
 
 #include <QApplication>
 #include <QCoreApplication>
 #include <QDebug>
 #include <QDirIterator>
-#include <QFile>
-#include <QTextStream>
 #include <QTranslator>
 
 // Nagłówki aplikacji
@@ -25,34 +23,15 @@
 #include "utils.h"
 
 /**
- * @brief Ładuje arkusz stylów z podanego pliku QSS.
- * @param filePath Ścieżka do pliku QSS w systemie zasobów Qt (np. ":/styles/default.qss").
- *
- * Otwiera plik QSS, wczytuje jego zawartość i ustawia styl aplikacji za pomocą qApp->setStyleSheet.
- * Loguje sukces lub błąd ładowania.
- */
-void loadStyleSheet(const QString &filePath)
-{
-    QFile file(filePath);
-    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        QString style = QString::fromUtf8(file.readAll());
-        qDebug() << "Zawartość pliku stylu (" << filePath << "):\n" << style;
-        qApp->setStyleSheet(style);
-        file.close();
-    } else {
-        qWarning() << "Nie można załadować pliku stylu:" << filePath;
-    }
-}
-
-/**
  * @brief Główna funkcja aplikacji inwentaryzacyjnej.
  * @param argc Liczba argumentów wiersza poleceń.
  * @param argv Tablica argumentów wiersza poleceń.
  * @return Kod zakończenia aplikacji (0 dla sukcesu, inne dla błędów).
  *
  * Inicjalizuje aplikację Qt, ustawia nazwę i wersję aplikacji, ładuje tłumaczenia na podstawie
- * języka systemowego, ładuje skórkę graficzną z QSettings, wyświetla okno dialogowe konfiguracji
- * bazy danych, nawiązuje połączenie z bazą danych (SQLite lub MySQL) i uruchamia główne okno aplikacji.
+ * języka systemowego, wyświetla okno dialogowe konfiguracji bazy danych, nawiązuje połączenie
+ * z bazą danych (SQLite lub MySQL) i uruchamia główne okno aplikacji. Skórka graficzna jest
+ * ładowana dynamicznie przez DatabaseConfigDialog po zatwierdzeniu ustawień.
  */
 int main(int argc, char *argv[])
 {
@@ -60,21 +39,8 @@ int main(int argc, char *argv[])
     // Wymuszenie stylu Qt, aby uniknąć nadpisywania przez styl systemowy
     a.setStyle("Fusion");
 
-    // Ładowanie skórki z QSettings
-    QSettings settings("SMOK", "Inwentaryzacja");
-    QString skin = settings.value("skin", "Standard").toString();
-    QString qssPath;
-    if (skin == "Amiga") {
-        qssPath = ":/styles/amiga.qss";
-    } else if (skin == "ZX Spectrum") {
-        qssPath = ":/styles/zxspectrum.qss";
-    } else {
-        qssPath = ":/styles/default.qss"; // Domyślnie Standard
-    }
-    loadStyleSheet(qssPath);
-
     QCoreApplication::setApplicationName(QStringLiteral("Inwentaryzacja"));
-    QCoreApplication::setApplicationVersion(QStringLiteral(APP_VERSION));
+    QCoreApplication::setApplicationVersion(QStringLiteral("1.2.0"));
 
     QIcon icon(":/images/icon.png");
     a.setWindowIcon(icon);
@@ -100,7 +66,7 @@ int main(int argc, char *argv[])
     // Pobranie typu bazy danych wybranego przez użytkownika
     QString dbType = configDlg.selectedDatabaseType();
     bool connected = false;
-    // zaakceptujmy wszystkie warianty SQLite (np. "SQLite", "SQLite3", ...)
+    // Akceptujmy wszystkie warianty SQLite (np. "SQLite", "SQLite3", ...)
     if (dbType.startsWith("SQLite", Qt::CaseInsensitive)) {
         QString filePath = configDlg.sqliteFilePath();
         if (filePath.isEmpty()) {
